52826e4e58ea078bebd52cb5671f046d0f24bde4
Oliver Caldwell <olliec87@gmail.com>
2012-08-03 22:19:17 +0100

    I am now extracting the values from the form and validating them.

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,36 @@ require([
      * Executed when the DOM is ready
      */
     function main() {
-        
+        // Find the elements
+        var elements = {
+            form: $('form'),
+            width: $('width'),
+            columns: $('columns'),
+            margin: $('margin'),
+            output: $('output')
+        };
+
+        // Listen for when the form is submitted
+        elements.form.addEvent('submit', function() {
+            // Extract the data
+            var config = {
+                width: elements.width.get('value').toInt(),
+                columns: elements.columns.get('value').toInt(),
+                margin: elements.margin.get('value').toInt()
+            };
+
+            // Validate all of the values
+            if(!isNaN(config.width) && !isNaN(config.columns) && !isNaN(config.margin)) {
+                // We have three valid values
+                // Set up a grid renderer
+            }
+            else {
+                alert('Please specify a number in each input.');
+            }
+
+            // Stop the form actually submitting
+            return false;
+        });
     }
 
     // Add a listener for the DOM ready event
