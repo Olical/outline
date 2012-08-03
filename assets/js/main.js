157bf3d9027c772f0ffb6af062245f0e60a57126
Oliver Caldwell <olliec87@gmail.com>
2012-08-03 21:29:15 +0100

    Loaded Class into main. Also changed mootools to mootools-core

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,12 @@
 requirejs.config({
     paths: {
-        mootools: 'https://ajax.googleapis.com/ajax/libs/mootools/1.4.5/mootools-yui-compressed'
+        'mootools-core': 'https://ajax.googleapis.com/ajax/libs/mootools/1.4.5/mootools-yui-compressed'
     }
 });
 
 require([
-    'mootools'
-], function() {
-    console.log('Hello, World!');
+    'Class',
+    'mootools-core'
+], function(Class) {
+
 });
