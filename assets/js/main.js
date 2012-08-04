a8c6e1b7a3bca9bcafb1207f7ad5311517a1e7c2
Oliver Caldwell <olliec87@gmail.com>
2012-08-04 22:33:17 +0100

    Included the URL with the hash on the end in the grid render.

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,6 +64,11 @@ require([
                 // Set up a grid renderer
                 grid.setOptions(config);
 
+                // Set the hash
+                // This is done before the grid is rendered
+                // So now the URL with the hash on the end can be in the file
+                window.location.hash = config.width + ',' + config.columns + ',' + config.margin;
+
                 // Render the CSS
                 grid.render(template, function(err, css) {
                     // Log any errors
@@ -81,9 +86,6 @@ require([
                     // Prepare the download and show the link
                     elements.download.set('href', 'data:text/css;charset=utf-8;base64,' + btoa(css));
                     elements.controls.removeClass('hidden');
-
-                    // Set the hash
-                    window.location.hash = config.width + ',' + config.columns + ',' + config.margin;
 
                     // And set the share URL
                     loc = document.location;
